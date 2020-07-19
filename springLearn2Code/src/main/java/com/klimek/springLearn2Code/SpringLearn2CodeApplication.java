e1e682e3d8302d01134c8402cb75170225fa573c
BaskaKlim <baska.klimekova@gmail.com>
2020-07-19 18:54:06 +0200

    take AppRun from Spring container and use it as a bean with run() method

--- springLearn2Code/src/main/java/com/klimek/springLearn2Code/SpringLearn2CodeApplication.java
+++ springLearn2Code/src/main/java/com/klimek/springLearn2Code/SpringLearn2CodeApplication.java
@@ -1,13 +1,25 @@
 package com.klimek.springLearn2Code;
 
+import org.apache.catalina.core.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.*;
 
 @SpringBootApplication
 public class SpringLearn2CodeApplication {
 
+	//TODO: vytiahnut AppRun zo Spring containera, aby sme to vedeli pouzit ako bean, nie vytvorit novu instanciu
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearn2CodeApplication.class, args);
+		//vytvorenie spring kontextu
+		ConfigurableApplicationContext kontext =
+			 SpringApplication.run(SpringLearn2CodeApplication.class, args);
+
+		//vytiahnutie beany
+		AppRun appRun = kontext.getBean(AppRun.class);
+		appRun.run();
+
+
 	}
 
 }
