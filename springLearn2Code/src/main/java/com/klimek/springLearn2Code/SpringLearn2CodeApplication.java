c894da3a85cd13e4f641630fe2a1635d6dff7ed3
BaskaKlim <baska.klimekova@gmail.com>
2020-07-19 19:10:44 +0200

    creating listener for contextRefreshedEvent and deleting context in SpeingLearn2CodeApplication

--- springLearn2Code/src/main/java/com/klimek/springLearn2Code/SpringLearn2CodeApplication.java
+++ springLearn2Code/src/main/java/com/klimek/springLearn2Code/SpringLearn2CodeApplication.java
@@ -11,13 +11,10 @@ public class SpringLearn2CodeApplication {
 	//TODO: vytiahnut AppRun zo Spring containera, aby sme to vedeli pouzit ako bean, nie vytvorit novu instanciu
 
 	public static void main(String[] args) {
-		//vytvorenie spring kontextu
-		ConfigurableApplicationContext kontext =
+	
 			 SpringApplication.run(SpringLearn2CodeApplication.class, args);
 
-		//vytiahnutie beany z spring kontexte {kontainera}
-		AppRun appRun = kontext.getBean(AppRun.class);
-		appRun.run();
+
 
 
 	}
--- springLearn2Code/src/main/java/com/klimek/springLearn2Code/initializaters/DataInit.java
+++ springLearn2Code/src/main/java/com/klimek/springLearn2Code/initializaters/DataInit.java
@@ -2,13 +2,21 @@ package com.klimek.springLearn2Code.initializaters;
 
 import org.springframework.context.*;
 import org.springframework.context.event.*;
+import org.springframework.stereotype.*;
 import com.klimek.springLearn2Code.services.*;
 
+@Component
 public class DataInit implements ApplicationListener<ContextRefreshedEvent> {
 
     //zavolam movieservice
-    
+
     MovieService movieService;
+
+    //konstruktor kde si zavolam rovno movieServisu
+
+    public DataInit(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
