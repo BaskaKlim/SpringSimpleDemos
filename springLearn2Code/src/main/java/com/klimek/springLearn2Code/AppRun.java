309f2952d1e3d8f1e87dc27a0de51dfec666c621
BaskaKlim <baska.klimekova@gmail.com>
2020-07-19 18:43:37 +0200

    got implementation of MovieService into support class AppRun by @Autowired anotation

--- springLearn2Code/src/main/java/com/klimek/springLearn2Code/AppRun.java
+++ springLearn2Code/src/main/java/com/klimek/springLearn2Code/AppRun.java
@@ -1,5 +1,6 @@
 package com.klimek.springLearn2Code;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import com.klimek.springLearn2Code.services.*;
 
@@ -7,10 +8,14 @@ import com.klimek.springLearn2Code.services.*;
 
 @Component
 public class AppRun {
+    //TODO: dotiahnut zo springu aj implementaciu movieService -> dependency injection
 
+    /* da sa to pomocou konstruktoru alebo pomocou @Autowired */
+
+    @Autowired
     MovieService movieService;
 
-    public void run(){
+    public void run() {
         movieService.createAndAddMovie();
     }
 
