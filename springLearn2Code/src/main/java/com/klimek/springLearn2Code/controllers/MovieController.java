81e5ef9ec011e2e4d5a3fb54abc4a2399b92f6d2
BaskaKlim <baska.klimekova@gmail.com>
2020-07-20 23:10:09 +0200

    injection of MovieRepository

--- springLearn2Code/src/main/java/com/klimek/springLearn2Code/controllers/MovieController.java
+++ springLearn2Code/src/main/java/com/klimek/springLearn2Code/controllers/MovieController.java
@@ -2,9 +2,20 @@ package com.klimek.springLearn2Code.controllers;
 
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import com.klimek.springLearn2Code.repositories.*;
 
 @Controller
 public class MovieController {
+
+    //injection of movieRepository by using constructor
+
+    MovieRepository movieRepository;
+
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
 
     @RequestMapping ("/movie")
     public String getMovies(){
