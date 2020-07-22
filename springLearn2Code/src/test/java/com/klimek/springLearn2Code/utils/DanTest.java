a6249ce037a46a43fbac5b9e57b6347cca22369e
BaskaKlim <baska.klimekova@gmail.com>
2020-07-22 23:39:24 +0200

    prepared SpringBootTest template for DanTest

---
+++ springLearn2Code/src/test/java/com/klimek/springLearn2Code/utils/DanTest.java
@@ -0,0 +1,14 @@
+package com.klimek.springLearn2Code.utils;
+
+import org.junit.runner.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+class DanTest {
+
+}
