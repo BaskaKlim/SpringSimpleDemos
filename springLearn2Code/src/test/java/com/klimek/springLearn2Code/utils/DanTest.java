271e6ac1caf717202817c6b1d3c8cc661e49b177
BaskaKlim <baska.klimekova@gmail.com>
2020-07-23 22:42:34 +0200

    Created unit Test in JUnit

--- springLearn2Code/src/test/java/com/klimek/springLearn2Code/utils/DanTest.java
+++ springLearn2Code/src/test/java/com/klimek/springLearn2Code/utils/DanTest.java
@@ -1,5 +1,7 @@
 package com.klimek.springLearn2Code.utils;
 
+import java.math.*;
+import org.junit.jupiter.api.*;
 import org.junit.runner.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.test.context.junit4.*;
@@ -7,8 +9,15 @@ import org.springframework.test.context.junit4.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
+
 @SpringBootTest
 
 class DanTest {
 
+    @Test
+    public void suma100PlusDanJe120(){
+       BigDecimal pripocitane =  Dan.pripocitajDan(BigDecimal.valueOf(100L));
+       assertEquals(BigDecimal.valueOf(120),pripocitane);
+                                                         
+    }
 }
